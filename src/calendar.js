595a028b9a9ecca0509d2385db2dbfdd0df4c7c0
Charles Feng <charles@charles.io>
2013-01-30 19:08:51 -0800

    add ability to change all important view classes

--- src/calendar.js
+++ src/calendar.js
@@ -16,21 +16,25 @@ Ember.Calendar.CalendarController = Ember.ArrayController.extend({
   , headingTimeRangeEnd: 24
   , eventTimeFormat: 'h:mm a'
   , eventTimeSeparator: ' - '
+  
+  , headingDateViewClass: 'Ember.Calendar.HeadingDateView'
+  , headingTimeViewClass: 'Ember.Calendar.HeadingTimeView'
+  , dayViewClass: 'Ember.Calendar.DayView'
   , eventViewClass: 'Ember.Calendar.EventView'
   
   , week: null
-  , days: function () {
+  , dates: function () {
       if (!this.get('week'))
         return []
       
       var curr = this.get('week').clone().subtract('days', 1)
-        , days = []
+        , dates = []
         , i
       
       for (i = 0; i < 7; i++)
-        days.push(curr.add('days', 1).clone())
+        dates.push(curr.add('days', 1).clone())
       
-      return days
+      return dates
     }.property('week')
   , times: function () {
       var times = []
@@ -42,12 +46,12 @@ Ember.Calendar.CalendarController = Ember.ArrayController.extend({
       return times
     }.property('headingTimeRangeStart', 'headingTimeRangeStart')
     
-  , events: function () {
+  , days: function () {
       if (!this.get('week'))
         return []
       
-      var events = []
-        , days = this.get('days')
+      var days = []
+        , dates = this.get('dates')
       
       this.get('content').forEach(function (event) {
         var start = moment(event.start).clone()
@@ -55,7 +59,7 @@ Ember.Calendar.CalendarController = Ember.ArrayController.extend({
           , object
           , day
         
-        if (end < days[0] || start > days[6])
+        if (end < dates[0] || start > dates[6])
           return
         
         while (end > start) {
@@ -73,14 +77,14 @@ Ember.Calendar.CalendarController = Ember.ArrayController.extend({
           
           day = object.start.clone().startOf('day').diff(this.get('week'), 'days')
           if (day >= 0 && day <= 6)
-            events[day].push(object)
+            days[day].push(object)
           
           start.add('days', 1).startOf('day')
         }
       })
       
-      return events
-    }.property('content', 'days')
+      return days
+    }.property('content', 'dates')
     
   , loadPreviousWeek: function () {
       this.get('week').subtract('days', 7)
@@ -105,28 +109,58 @@ Ember.Calendar.CalendarView = Ember.View.extend({
     classNames: ['ember-calendar']
 })
 
-Ember.Calendar.HeadingDayView = Ember.View.extend({
-    classNames: ['ember-calendar-head-day']
+Ember.Calendar.HeadingDatesView = Ember.ContainerView.extend({
+    classNames: ['ember-calendar-head-dates']
+  , childViews: function () {
+      var self = this
+      return this.get('dates').map(function (date) {
+        return Ember.create(self.get('parentView.controller.headingDateViewClass'), { date: date })
+      })
+    }.property('dates')
+})
+
+Ember.Calendar.HeadingDateView = Ember.View.extend({
+    templateName: 'ember-calendar-head-date'
+  , classNames: ['ember-calendar-head-date']
   , dateString: function () {
-      return this.get('context').format(this.get('parentView.controller.headingDateFormat'))
-    }.property('context')
+      return this.get('date').format(this.get('parentView.parentView.controller.headingDateFormat'))
+    }.property('date')
+})
+
+Ember.Calendar.HeadingTimesView = Ember.ContainerView.extend({
+    classNames: ['ember-calendar-head-times']
+  , childViews: function () {
+      var self = this
+      return this.get('times').map(function (time) {
+        return Ember.create(self.get('parentView.controller.headingDateViewClass'), { time: time })
+      })
+    }.property('times')
 })
 
 Ember.Calendar.HeadingTimeView = Ember.View.extend({
-    classNames: ['ember-calendar-head-time']
+    templateName: 'ember-calendar-head-time'
+  , classNames: ['ember-calendar-head-time']
   , timeString: function () {
-      return this.get('context').format(this.get('parentView.controller.headingTimeFormat'))
-    }.property('context')
+      return this.get('time').format(this.get('parentView.parentView.controller.headingTimeFormat'))
+    }.property('time')
+})
+
+Ember.Calendar.DaysView = Ember.ContainerView.extend({
+    classNames: ['ember-calendar-days']
+  , childViews: function () {
+      days.forEach(function (events) {
+        return Ember.create(self.get('parentView.controller.dayViewClass', { events: events }))
+      })
+    }.property('days')
 })
 
 Ember.Calendar.DayView = Ember.ContainerView.extend({
     classNames: ['ember-calendar-day']
   , childViews: function () {
-      var self = this
-      return this.get('context').map(function (event) {
-        return Ember.create(self.get('parentView.controller.eventViewClass'), { event: event })
+      events.forEach(function (event) {
+        return Ember.create(self.get('parentView.parentView.controller.eventViewClass', { event: event }))
       })
-    }.property('context')
+    }.property('events')
 })
 
 Ember.Calendar.EventView = Ember.View.extend({
@@ -140,4 +174,16 @@ Ember.Calendar.EventView = Ember.View.extend({
       
       return 'top: ' + (start - rangeStart) / (rangeEnd - rangeStart) + '%; height: ' + (end - start) / (rangeEnd - rangeStart) + '%;'
     }
+  , nameString: function () {
+      if (!this.get('event')) return ''
+      return this.get('event.name')
+    }.property('event', 'event.name')
+  , timeString: function () {
+      if (!this.get('event')) return ''
+      return this.get('event.start').format(this.get('parentView.parentView.parentView.controller.eventTimeFormat')) + this.get('parentView.parentView.parentView.controller.eventTimeSeparator') + this.get('event.end').format(this.get('parentView.parentView.parentView.controller.eventTimeFormat'))
+    }.property('event', 'event.start', 'event.end')
+  , locationString: function () {
+      if (!this.get('event')) return ''
+      return this.get('event.location.name') || this.get('event.location.address')
+    }.property('event', 'event.location')
 })
