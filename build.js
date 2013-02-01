aba3f66c1801b096c7642a1eb8d514d3f43b9012
Charles Feng <charles@charles.io>
2013-01-31 17:08:49 -0800

    delete gitignore; update build.js to save as dist/ember-*

--- build.js
+++ build.js
@@ -12,8 +12,8 @@ var handlebars = cat('src/calendar.html').split('</script>').slice(0, -1).map(fu
 	return 'Ember.TEMPLATES["' + name + '"]=Ember.Handlebars.compile(' + source + ');\n'
 }).join('')
 
-cat('src/calendar.css').to('dist/calendar.css');
-(handlebars + cat('src/calendar.js')).to('dist/calendar.js')
+cat('src/calendar.css').to('dist/ember-calendar.css');
+(handlebars + cat('src/calendar.js')).to('dist/ember-calendar.js')
 
-exec('yuicompressor dist/calendar.css').output.to('dist/calendar.min.css')
-exec('closure-compiler --language_in ECMASCRIPT5 --js dist/calendar.js', { silent: true }).output.to('dist/calendar.min.js')
+exec('yuicompressor dist/ember-calendar.css', { silent: true }).output.to('dist/ember-calendar.min.css')
+exec('closure-compiler --language_in ECMASCRIPT5 --js dist/ember-calendar.js', { silent: true }).output.to('dist/ember-calendar.min.js')
