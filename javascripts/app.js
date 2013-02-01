facf10683e86c9da403baa2fdb16e3401c1e6fb6
Charles Feng <charles@charles.io>
2013-01-31 21:52:21 -0800

    initial page commit

---
+++ javascripts/app.js
@@ -0,0 +1,41 @@
+///////////////////////////////////////////////////////////////////////////////
+// Application
+///////////////////////////////////////////////////////////////////////////////
+App = Ember.Application.create()
+
+App.ApplicationController = Ember.Controller.extend({
+    needs: ['simpleCalendar']
+})
+
+App.ApplicationView = Ember.View.extend({
+    templateName: 'application'
+})
+
+App.initialize()
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Controller
+///////////////////////////////////////////////////////////////////////////////
+App.SimpleCalendarController = Ember.Calendar.CalendarController.extend({
+    content: function () {
+      var events = []
+        , date
+        , time
+        , duration
+
+      for (var i = 0; i < 10; i++) {
+        date = Math.floor(Math.random() * 7)
+        time = 1000 * 60 * 60 * 8 + 1000 * 60 * 30 * Math.floor(Math.random() * 24)
+        duration = 1000 * 60 * 30 * (1 + Math.floor(Math.random() * 5))
+        
+        events.push({
+            name: 'Event ' + events.length
+          , start: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time)
+          , end: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time + duration)
+        })
+      }
+      
+      return events
+    }.property()
+})
