f79671782b681433826fccde9ca099884159bdb0
Teddy Cross <tkazec@gmail.com>
2013-01-31 17:23:30 -0800

    add license line to built files

--- build.js
+++ build.js
@@ -3,6 +3,8 @@ require('shelljs/global')
 rm('-r', 'dist')
 mkdir('dist')
 
+var license = '/* ember-calendar (https://github.com/joinspoton/ember-calendar) | (c) 2013 SpotOn (https://spoton.it) | http://www.opensource.org/licenses/MIT */\n'
+
 var handlebars = cat('src/calendar.html').split('</script>').slice(0, -1).map(function (source) {
 	var name = source.match(/data-template-name=\'(.*)\'/)[1]
 	
@@ -12,8 +14,8 @@ var handlebars = cat('src/calendar.html').split('</script>').slice(0, -1).map(fu
 	return 'Ember.TEMPLATES["' + name + '"]=Ember.Handlebars.compile(' + source + ');\n'
 }).join('')
 
-cat('src/calendar.css').to('dist/ember-calendar.css');
-(handlebars + cat('src/calendar.js')).to('dist/ember-calendar.js')
+;(license + cat('src/calendar.css')).to('dist/ember-calendar.css')
+;(license + handlebars + cat('src/calendar.js')).to('dist/ember-calendar.js')
 
-exec('yuicompressor dist/ember-calendar.css', { silent: true }).output.to('dist/ember-calendar.min.css')
-exec('closure-compiler --language_in ECMASCRIPT5 --js dist/ember-calendar.js', { silent: true }).output.to('dist/ember-calendar.min.js')
+;(license + exec('yuicompressor dist/ember-calendar.css', { silent: true }).output).to('dist/ember-calendar.min.css')
+;(license + exec('closure-compiler --language_in ECMASCRIPT5 --js dist/ember-calendar.js', { silent: true }).output).to('dist/ember-calendar.min.js')
