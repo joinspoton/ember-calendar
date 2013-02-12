4fdb058138edf3711e06bc1d919b900df1e6d2e1
Charles Feng <charles@charles.io>
2013-02-12 15:09:19 -0800

    ajax example

---
+++ examples/ajax/app.js
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////////////////////////
+// Application
+///////////////////////////////////////////////////////////////////////////////
+App = Ember.Application.create()
+
+App.ApplicationController = Ember.Controller.extend({
+    needs: ['calendar']
+})
+
+App.ApplicationView = Ember.View.extend({
+    templateName: 'application'
+})
+
+App.initialize()
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Controller
+///////////////////////////////////////////////////////////////////////////////
+App.CalendarController = Ember.Calendar.CalendarController.extend({
+    content: []
+  , update: function () {
+      if (!this.get('week')) return
+      
+      var self = this
+      $.getJSON('http://ember-calendar-ajax.herokuapp.com', { week: self.get('week').toDate() }, function (response) {
+        if (!response) return
+        self.clear().pushObjects(response).notifyPropertyChange('content')
+      })
+    }.observes('week')
+})
